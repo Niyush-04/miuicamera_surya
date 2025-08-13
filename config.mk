787985d5098f3f6280647ea95779eda108f5a87b
Pranav Vashi <neobuddy89@gmail.com>
2025-08-13 21:31:31 +0530

    miuicamera: Set camera overrides via soong config
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- config.mk
+++ config.mk
@@ -11,4 +11,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.lens.oem_camera_package=com.android.camera
 
+$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call soong_config_set,camera,package_name,com.xiaomi.sessionparams.clientName)
+
 include vendor/xiaomi/miuicamera/BoardConfig.mk
