ae78b96ea53c4df90a18466ce2e3c91e9dbd8fcd
Niyush-04 <mauryapradip04@gmail.com>
2025-11-26 22:01:01 +0530

    surya

--- config.mk
+++ config.mk
@@ -4,6 +4,11 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc)
 
+# Surya blobs
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
+    vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiCamera/lib/arm64/libcamera_algoup_jni.xiaomi.so:$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiCamera/lib/arm64/libcamera_algoup_jni.xiaomi.so
+
 PRODUCT_PACKAGES += \
     MiuiCamera
 
